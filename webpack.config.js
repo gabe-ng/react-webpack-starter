d663e757ae575633d454d4c2c5bf91931e6824c6
Gabriel Ng <gabrielng15@gmail.com>
2018-11-09 16:42:51 -0800

    Updated all old babel files to 7.0+ (#6)

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,31 @@
-const path = require();
+const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = {
+module.exports = (env, options) => {
     // Javascript entry file
-    entry: './src/index.js',
-    // Where our compiled code will go
-    output: {
-        path: path.join(__dirname, '/dist'),
-        filename: 'index_bundle.js',
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
+    return { 
+        entry: './src/index.js',
+        // Where our compiled code will go
+        output: {
+            path: path.join(__dirname, '/dist'),
+            filename: 'index_bundle.js',
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: 'babel-loader',
+                    }
                 }
-            }
+            ]
+        },
+        plugins: [
+            // Specify index.html as template
+            new HtmlWebpackPlugin({
+                template: './src/public/index.html'
+            })
         ]
-    },
-    plugins: [
-        // Specify index.html as template
-        new HtmlWebpackPlugin({
-            template: '../src/public/index.html'
-        })
-    ]
+    }   
 }
